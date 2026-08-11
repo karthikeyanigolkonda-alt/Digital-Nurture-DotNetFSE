56b57bed82a6d14b782eb1d04d29089b8883ee94
karthikeyanigolkonda-alt <karthikeyanigolkonda@gmail.com>
2026-08-11 15:39:06 +0530

    Complete C# ADO.NET upskilling

---
+++ Upskilling/Module 3-C#ADO.NET/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+string connectionString =
+    "Server=localhost;Database=StudentCoursePortal;Trusted_Connection=True;TrustServerCertificate=True;";
+
+using SqlConnection connection = new SqlConnection(connectionString);
+
+try
+{
+    connection.Open();
+
+    Console.WriteLine("Connected to SQL Server successfully!");
+
+    string query = "SELECT GETDATE()";
+
+    using SqlCommand command = new SqlCommand(query, connection);
+
+    object result = command.ExecuteScalar();
+
+    Console.WriteLine("Server Date and Time: " + result);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error: " + ex.Message);
+}
